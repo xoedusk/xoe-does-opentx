760a58d2ac5038f6b9340d83d44bd3cad0a9e15f
Brian Stephanik <xoedusk@gmail.com>
2020-02-03 12:23:13 -0800

    Update and rename addtim.lua to addTim.lua
    Better instructions.

--- Lua Scripts/addTim.lua
+++ Lua Scripts/addTim.lua
@@ -1,16 +1,24 @@
+-- addTim.lua
+-- Purpose: In OpenTX function scripts, adds a number of seconds to the specified timer.
+-- 
+-- (C) Brian Stephanik 2020
+-- Released under GPL-3.0-or-later
 --
--- INSTRUCTIONS
--- Step 1: Change the user-defined options below to suit your needs
--- Step 2: This Function Script should be called using a Logical Switch of type Edge.
--- For the Edge logical switch, try using: Edge, <switch of your choice>, 0.0, (instant)
+-- INSTRUCTIONS FOR USE
+-- Step 1: Change the USER-DEFINED OPTIONS below to suit your needs
+-- Step 2: Place this addTim.lua file into your TX's SD card in SCRIPTS/FUNCTIONS/
+-- Step 3: In your OpenTX model, set up an Edge-type logical switch that will turn true for a short time
+--         when the switch/button of your choice is pressed: Edge, <button choice>, 0.0:<< (instant)
+-- Step 4: In your OpenTX model, create a special function that triggers from the Edge switch.
+-- Step 5: Press button to add time!
 
 
--- USER-DEFINED OPTIONS --
+-- USER-DEFINED OPTIONS --      -- Edit these to your liking.
 local TIMER_INDEX = 0		-- "0" is Timer1, "1" is Timer2, "2" is Timer3. Default is 0
-local numSecondsToAdd = 15	-- num seconds to add to selected timer. Default is 15.
+local numSecondsToAdd = 15	-- num seconds to add to selected timer. Default is 15 (seconds).
 -- END USER-DEFINED OPTIONS --
 
-local t_next = 0  												-- Earliest time at which next run_func() is allowed.
+local t_next = 0  								-- Earliest time at which next run_func() is allowed.
 
 local timerToAdjust = {}  										-- will hold reference to the active timer.
 local  newTimeValue  											-- integer in seconds for what the new timer value should be
